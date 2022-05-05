7bfa91280945818cfa9f59262de79c52d5601135
Adrian Delgado <adrian@adelgado.net>
2022-05-05 14:43:39 +0200

    Commit 050522

---
+++ aboutUs.php
@@ -0,0 +1,200 @@
+<?php session_start()?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />    
+    <meta name="author" content="Angel Criado, Adrian Delgado y Sergio Jimenez" />    
+	<meta name="description" content="Tienda
+ MDLR">
+    <link rel="icon" type="image/png" href="images/icono.png">   
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Document title -->
+    <title>MDLR | About Us</title>
+    <!-- Stylesheets & Fonts -->
+    <link href="css/plugins.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Body Inner -->
+    <div class="body-inner">
+        <!-- Header -->
+        <?php
+            include("header.php");
+        ?>
+        <!-- end: Header -->
+        <!-- Page title -->
+        <section id="page-title" data-bg-parallax="images/clients/cieloazul.jpg">
+            <div class="bg-overlay"></div>
+            <div class="container">
+                <div class="page-title">
+                <h1 class="text-uppercase text-medium">SOBRE NOSOTROS</h1>
+                    <span>De Madrid al cielo since 2022!</span>
+                </div>
+            </div>
+        </section>
+        <!-- end: Page title -->
+        <section class="p-b-10">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="heading-text heading-section">
+                            <h2>LA EMPRESA</h2>
+                            <span class="lead">MDLR es un proyecto nacido de la idea de dos jóvenes programadores, apasionados por el mundo del streetwear , con unas ganas infinitas de crear un proyecto sólido, apasionante en el cual hemos volcado todo nuestro esfuerzo y nuestra ilusión. Por ello, en MDLR hemos confeccionado un proyecto ambicioso aportando nuestros conocimientos acerca del mundo del streetwear, ayudados por nuestro saber en el mundo de la programación y el diseño web. Esperamos crecer junto a todos vosotros en este apasionante proyecto. </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="background-grey">
+            <div class="container">
+                <div class="heading-text heading-section text-center">
+                    <h2>STAFF</h2>
+                    <span class="lead">¡Conocenos en detalle!</span>
+                </div>
+                <div class="row team-members team-members-shadow m-b-40">
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <div class="team-image">
+                                <img src="images/clients/adrian2.jpg">
+                            </div>
+                            <div class="team-desc">
+                                <h3>Adrián Delgado</h3>
+                                <span>Coofundador</span>
+                                <p>Ideólogo de MDLR. Entre sus características destaca su inteligencia, adaptabilidad al cambio, resolutivo y con inmensas ganas de crecer y mejorar </p>
+                                <div class="align-center">
+                                    <a class="btn btn-xs btn-slide btn-light" href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                        <span>Facebook</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+                                        <i class="fab fa-twitter"></i>
+                                        <span>Twitter</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+                                        <i class="fab fa-instagram"></i>
+                                        <span>Instagram</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+                                        <i class="icon-mail"></i>
+                                        <span>Mail</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <div class="team-image">
+                                <img src="images/clients/prueba.png">
+                            </div>
+                            <div class="team-desc">
+                                <h3>Ángel Criado</h3>
+                                <span>Coofundador</span>
+                                <p>Ideólogo de MDLR. Destaca por sus grandes capacidades para la innovación tanto al uso de funcionalidades como al modelo de negocio.</p>
+                                <div class="align-center">
+                                    <a class="btn btn-xs btn-slide btn-light" href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                        <span>Facebook</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+                                        <i class="fab fa-twitter"></i>
+                                        <span>Twitter</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+                                        <i class="fab fa-instagram"></i>
+                                        <span>Instagram</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+                                        <i class="icon-mail"></i>
+                                        <span>Mail</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <div class="team-image">
+                                <img src="images/clients/prueba.png">
+                            </div>
+                            <div class="team-desc">
+                                <h3>Sergio Jimenez</h3>
+                                <span>Coofundador</span>
+                                <p>Ideólogo de MDLR. Se define como una persona con capacidad de liderazgo, inteligente, trabajador y sobretodo gran compañero de sus socios Adrián y Ángel.</p>
+                                <div class="align-center">
+                                    <a class="btn btn-xs btn-slide btn-light" href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                        <span>Facebook</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+                                        <i class="fab fa-twitter"></i>
+                                        <span>Twitter</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+                                        <i class="fab fa-instagram"></i>
+                                        <span>Instagram</span>
+                                    </a>
+                                    <a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+                                        <i class="icon-mail"></i>
+                                        <span>Mail</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="seperator"><i class="fa fa-chevron-down"></i>
+        </div>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <p class="lead">¡Nuestras increíbles marcas con las que tenemos el placer de trabajar!</p>
+                    </div>
+                </div>
+                <ul class="grid grid-4-columns">
+                    <li>
+                        <a href="#"><img src="images/clients/casablanca.jpg" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="images/clients/gucci.jpg" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="images/clients/off-white.jpg" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="images/clients/stussy.jpg" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <!-- Footer -->
+    <br>
+    <footer id="footer">
+        <div class="copyright-content">
+            <div class="container">
+                <div class="copyright-text text-center">© 2022 MDLR - Hecho con amor y cariño en Madrid.</div>
+            </div>
+        </div>
+    </footer>
+    <!-- end: Footer -->
+    </div> <!-- end: Body Inner -->
+    <!-- Scroll top -->
+    <a id="scrollTop"><i class="icon-chevron-up"></i><i class="icon-chevron-up"></i></a>
+    <!--Plugins-->
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.js"></script>
+    <!--Template functions-->
+    <script src="js/functions.js"></script>
+</body>
+
+</html>
